16e7db523135fcdcabfddaf0e1f188835f6e82d6
ZeroGround <75402054+zerogroundebooks@users.noreply.github.com>
2024-01-27 02:42:10 -0300

    Delete js/cursor.js
    delete

--- js/cursor.js
+++
@@ -1,9 +0,0 @@
-const customCursor = document.getElementById('custom-cursor');
-
-
-const updateCursorPosition = (event) => {
-  customCursor.style.top = `${event.clientY}px`;
-  customCursor.style.left = `${event.clientX}px`;
-}
-
-
